7fbdd9b60052f28309be7b1c0256fc500e5b3b9f
Uriel Avalos <uriel.avalos@match.com>
2017-05-04 23:14:38 -0500

    start of entry point

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import createSagas from './stores/createSagas'
+import createStore from './stores/createStore'
+import StateWithSideEffects from './stores/StateWithSideEffects'
+import {state} from './stores/StateWithSideEffects'
