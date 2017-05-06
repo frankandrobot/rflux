efb2dc4b721a7c4cc42ad3fa539fece5bd5b0d7e
Uriel Avalos <uriel@rstudio.com>
2017-05-05 23:19:26 -0500

    [test] added kefir demo tests

--- tests/kefir-demo.test.js
+++ tests/kefir-demo.test.js
@@ -6,38 +6,172 @@ import test from 'tape'
 
 import kefir from 'kefir'
 
+import kefirEmitter from '../src/utils/kefirEmitter'
 
-test('scan outputs once', function(t) {
-  t.plan(1)
-  kefir.constant(1)
-    .scan((total, i) => t.equal(i, 1), 0)
-    .onValue(() => undefined)
+
+test('kefir#scan fires extra event for initialValue', function(t) {
+  t.plan(3)
+
+  let ctr = 0
+  const sequence = kefir.sequentially(50, [1, 2])
+
+  sequence.scan((_, i) => i, 0)
+    .onValue(x => {
+      switch(ctr++) {
+        case 0:
+          t.equal(x, 0) // initial value
+          break
+        case 1:
+          t.equal(x, 1)
+          break
+        case 2:
+          t.equal(x, 2)
+          break
+        default:
+          t.equal(0, 1) //should never be called
+      }
+    })
 })
 
-test('combine scan outputs XXX', function(t) {
-  t.plan(3)
-  let ctr = 0
-  // both start at the same time but the first one is faster
-  const obs1 = kefir.sequentially(50, [0, 1]).delay(50)
-  const obs2 = kefir.sequentially(100, [2, 3])
 
-  kefir.combine([obs1, obs2], (a, b) => {
-    switch(++ctr) {
-      case 1:
-        t.deepEqual({a, b}, {a: 0, b: 2})
-        break
-      case 2:
-        t.deepEqual({a, b}, {a: 1, b: 2})
-        break
-      case 3:
-        t.deepEqual({a, b}, {a: 1, b: 3})
+test('kefir#pool + kefir#scan autofires event for initialValue', function(t) {
+  t.plan(1)
+
+  let ctr = 0
+  const emitter = kefirEmitter()
+  const evenScan = emitter
+    .filter(x => x % 2 === 0)
+    .scan((_, x) => x, 0)
+
+  evenScan
+    .onValue(x => {
+      switch(ctr++) {
+        case 0:
+          t.equal(x, 0) // initial value
+          break
+        default:
+          t.equal(0, 1) //should never be called
+      }
+    })
+})
+
+test('kefir#pool + kefir#scan should work', function(t) {
+  t.plan(2)
+
+  let ctr = 0
+  const emitter = kefirEmitter()
+  const evenScan = emitter
+    .filter(x => x % 2 === 0)
+    .scan((_, x) => x, 0)
+
+  evenScan
+    .onValue(x => {
+      switch(ctr++) {
+        case 0:
+          t.equal(x, 0) // initial value
+          break
+        case 1:
+          t.equal(x, 2)
+          break
+        default:
+          t.equal(0, 1) //should never be called
+      }
+    })
+
+  emitter.emit(1)
+  emitter.emit(2)
+})
+
+
+test('kefir#pool + kefir#combined fires when all observables have values', function(t) {
+  t.plan(1)
+
+  const emitter = kefirEmitter()
+  const even = emitter.filter(x => x % 2 === 0)
+  const odd = emitter.filter(x => x % 2 === 1)
+  const observables = [even, odd]
+  const names = ['even', 'odd']
+
+  kefir.combine(
+    observables,
+    (...observables) => observables.reduce(
+      (combinedState, x, i) => Object.assign(
+        combinedState,
+        {[names[i]]: x}
+      ),
+      {}
+    )
+  ).onValue(state => {
+    t.deepEqual(state, {odd: 1, even: 0})
+  })
+
+  emitter.emit(0)
+  emitter.emit(1)
+})
+
+
+test('kefir#pool + kefir#combined + kefir#scan autofires initial state', function(t) {
+  t.plan(1)
+
+  let ctr = 0
+  const emitter = kefirEmitter()
+  const evenScan = emitter.filter(x => x % 2 === 0).scan((_, x) => x, 0)
+  const oddScan = emitter.filter(x => x % 2 === 1).scan((_, x) => x, 1)
+  const observables = [evenScan, oddScan]
+  const names = ['even', 'odd']
+
+  kefir.combine(
+    observables,
+    (...observables) => observables.reduce(
+      (combinedState, x, i) => Object.assign(
+        combinedState,
+        {[names[i]]: x}
+      ),
+      {}
+    )
+  ).onValue(state => {
+    switch(ctr++) {
+      case 0:
+        t.deepEqual(state, {even: 0, odd: 1})
         break
       default:
-        // will actually throw an error if called (since the plan is only for 3 calls
-        t.equal(1, 1)
+        t.equal(0, 1) // should not fire
     }
-  }).onValue(() => undefined)
+  })
 })
 
-const obs1 = kefir.sequentially(50, [0, 1]).delay(50)
-const obs2 = kefir.sequentially(100, [2, 3])
+
+test('kefir#pool + kefir#combined + kefir#scan should work initial state', function(t) {
+  t.plan(2)
+
+  let ctr = 0
+  const emitter = kefirEmitter()
+  const evenScan = emitter.filter(x => x % 2 === 0).scan((_, x) => x, 0)
+  const oddScan = emitter.filter(x => x % 2 === 1).scan((_, x) => x, 1)
+  const observables = [evenScan, oddScan]
+  const names = ['even', 'odd']
+
+  kefir.combine(
+    observables,
+    (...observables) => observables.reduce(
+      (combinedState, x, i) => Object.assign(
+        combinedState,
+        {[names[i]]: x}
+      ),
+      {}
+    )
+  ).onValue(state => {
+    switch(ctr++) {
+      case 0:
+        t.deepEqual(state, {even: 0, odd: 1})
+        break
+      case 1:
+        t.deepEqual(state, {even: 0, odd: 3})
+        break
+      default:
+        t.equal(0, 1)
+    }
+  })
+
+  emitter.emit(3)
+})
