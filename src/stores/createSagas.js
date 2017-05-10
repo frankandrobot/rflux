daaaa9afc0217f7569792780e8f52147c8659b9e
Uriel Avalos <uriel@rstudio.com>
2017-05-09 23:44:03 -0500

    [saga] new support

--- src/stores/createSagas.js
+++ src/stores/createSagas.js
@@ -1,6 +1,7 @@
 import assert from '../internal/assert'
 
 import {bindActionFunctions} from './createStore'
+import sagaFactory from './sagaFactory'
 
 
 export function bindSagaHandler(channel, sagaName, sagaHandler) {
@@ -42,31 +43,42 @@ function _bindSagaResultObservables(sagas) {
 }
 
 /**
-  * SideEffectActionFunctions are optional but if you pass these,
-  * then every SideEffect must have a corresponding action function.
-  *
-  * @param channel
-  * @param ActionTypes - map whose keys are the names of the side effects
-  * @param SagaActionFunctions - (optional) map of action functions
-  * @param SagaHandlers - map of handler functions
-  */
-export default function createSagas({channel, ActionTypes, SagaActionFunctions, SagaHandlers}) {
+ * Sagas are primarily used for handling ajax workflows.
+ *
+ * @param {String} channel - the name of the saga collection
+ * @param {Map<String,*>} ActionTypes - the names of the action types (aka side effects) these sagas handle
+ * @param {Map<ActionType,Function>} SagaActionFunctions - (optional) map of action functions indexed by
+ * ActionType. Like a store's action functions, these functions can be used to initiate a saga. Note
+ * that if you include one action function, then every ActionType must have a corresponding action function.
+ * @param {Function} SagaHandlersFn - higher order function with signature `({sagas})=>SagaHandlers` that
+ * accepts the `sagas` interface object and returns the SagaHandlers. The SagaHandlers are a map of
+ * functions indexed by ActionType i.e, Map<ActionType,Function>.
+ * @return {Saga} higher order function that creates the saga.
+ */
+export default function createSagas({channel, ActionTypes, SagaActionFunctions = {}, SagaHandlersFn}) {
 
   assert(typeof channel === 'string', 'Needs a channel and it needs to be a string')
   assert(ActionTypes, 'Need ActionTypes')
-  assert(SagaHandlers, 'Need SagaHandlers')
-
-  //every side effect must map to an action function and handler
-  Object.keys(ActionTypes).forEach(action => {
-    if (SagaActionFunctions) {
-      assert(SagaActionFunctions[action], `Channel ${channel} is missing side effect action function "${action}"`)
-    }
-    assert(SagaHandlers[action], `Channel ${channel} is missing side effect handler "${action}"`)
-  })
-
-  SagaActionFunctions = SagaActionFunctions || {}
+  assert(SagaHandlersFn, 'Need SagaHandlersFn')
+  assert(typeof SagaHandlersFn === 'function', 'SagaHandlersFn should be a higher order function')
 
   return AppDispatcher => {
+
+    const sagas = sagaFactory(AppDispatcher)
+    /* eslint-disable new-cap */
+    const SagaHandlers = SagaHandlersFn({sagas})
+    /* eslint-enable */
+
+    //every side effect must map to an action function and handler
+    Object.keys(ActionTypes).forEach(action => {
+      if (SagaActionFunctions) {
+        assert(
+          SagaActionFunctions[action],
+          `Channel ${channel} is missing side effect action function "${action}"`
+        )
+      }
+      assert(SagaHandlers[action], `Channel ${channel} is missing side effect handler "${action}"`)
+    })
 
     const observables = _bindSagaHandlers(channel, ActionTypes, SagaHandlers)(AppDispatcher)
 
--- src/stores/sagaFactory.js
+++ src/stores/sagaFactory.js
@@ -47,6 +47,8 @@ export default function sagaFactory(AppDispatcher) {
   }
 
 
+  //function select()
+
   return {
     sideEffects,
     put,
